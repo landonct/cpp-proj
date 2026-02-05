3de61d1d3df71ab2603f62eefe1f2b976fbea062
landonct <120686608+landonct@users.noreply.github.com>
2026-02-04 19:51:56 -0500

    Write print method in row struct

--- load_data.cpp
+++ load_data.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <ctime>
+#include <chrono>
+#include <string>
 
 struct Row
 {
     std::string symbol {};
-    std::time_t timestamp {};
+    std::time_t timestamp {}; // Use <chrono> instead
     double open {};
     // double high {};
     // double low {};
     // double close {};
     int volume {}; 
     // int trade_index {};
+
+    void print() { // Add parameter to change where print output goes
+        std::cout << "Symbol: " << symbol << "\n" << 
+        "Price opened at " << open << " at exactly " <<
+        std::chrono::system_clock::time_point(std::chrono::seconds(timestamp)) << ". Total volume was " << volume << "\n";
+    }
 };
 
 int main()
 {
     Row row1 {"AAPL", 1770093451, 270.1, 100};
-    std::vector<std::string> vec {};
+    std::vector<Row> vec {};
 
-    std::cout << row1.symbol;
+    row1.print();
 
     return 0;
 }
